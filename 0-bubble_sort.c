07b73ef7f1ad8f9309148ab1d8c4454014784941
Brianmanoti <Brianmanoti254@gmail.com>
2023-04-25 05:17:30 -0700

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,50 +1,25 @@
 #include <stdio.h>
-#include "sort.h"
-#include <stdlib.h>
+#include <sort.h>
 
 /**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
+ * bubble_sort - function that sorts numbers in ascending order
+ * @array: Array to be sorted
+ * @size: Size of the array
  */
-void print_array(const int *array, size_t size)
-{
-    size_t i;
-
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
-}
-/**
- * bubble_sort - function that takes to parameters
- * @int *array: a pointer to the first element of the array to be sorted.
- * @size_t size: the size of the array.
- */
-
 void bubble_sort(int *array, size_t size)
 {
-    unsigned int i, j;
-    int temp;
+	int temp;
 
-   for (i = 0; i < size - 1; i++)
-    {
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                print_array(array, size);
-            }
-        }
-    }
+	for (size_t i = 0; i < size - 1; i++)
+	{
+		for (size_t j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}
+	}
 }
-
