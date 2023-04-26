b00721533b77a9a73903e587c5b0bb166120a404
Brianmanoti <Brianmanoti254@gmail.com>
2023-04-26 04:59:40 -0700

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,24 +1,30 @@
-#include <stdio.h>
-#include <sort.h>
+#include "sort.h"
 
 /**
- * bubble_sort - function that sorts numbers in ascending order
- * @array: Array to be sorted
- * @size: Size of the array
+ * bubble_sort - sorts an array of integers in ascending order using the
+ *               Bubble sort algorithm
+ *
+ * @array: Pointer to the array to be sorted
+ * @size: Size of the array to be sorted
  */
 void bubble_sort(int *array, size_t size)
 {
-	int temp;
+	size_t i, j;
+	int tmp;
 
-	for (size_t i = 0; i < size - 1; i++)
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
 	{
-		for (size_t j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				temp = array[j];
+				tmp = array[j];
 				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				array[j + 1] = tmp;
+				print_array(array, size);
 			}
 		}
 	}
--- sort.h
+++ sort.h
@@ -1,9 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 
-void print_list(const listint_t *list);
-void print_array(const int *array, size_t size);
-
+#include <stdlib.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -13,9 +11,25 @@ void print_array(const int *array, size_t size);
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void bubble_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+listint_t *swap_node(listint_t *node, listint_t **list);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
+#endif
